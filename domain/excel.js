0a279d5c6c886962e2d0288384dccaf20afa24bf
SeiYukinari <sei.yukinari@nextremer.com>
2017-04-18 12:45:26 +0900

    改行のチェックを追加

--- domain/excel.js
+++ domain/excel.js
@@ -6,41 +6,41 @@ const SEPARATOR = "###";
 
 class Excel {
   constructor() {
-    this.book = xlsx.readFile( config.excel.InputExcelFilePath );
+    this.book = xlsx.readFile(config.excel.InputExcelFilePath);
     this.sheet = this.book.Sheets[config.excel.SheetName];
   }
 
   exec() {
     const excelData = this.getExcelData();
-    this.createJsonFile( excelData );
+    this.createJsonFile(excelData);
   }
 
   getExcelData() {
     const range = this.sheet['!ref'];
-    console.log( 'range:' + range );
-    const rangeVal = xlsx.utils.decode_range( range );
+    console.log('range:' + range);
+    const rangeVal = xlsx.utils.decode_range(range);
     const maxColumnNumber = this.getMaxColumnNumber();
 
     let map = new Map();
     let text = '';
-    for ( let row = rangeVal.s.r; row <= rangeVal.e.r; row++ ) {
+    for (let row = rangeVal.s.r; row <= rangeVal.e.r; row++) {
       //見出し行はとばす
-      if ( row <= config.excel.HeadRowNumber ) {
+      if (row <= config.excel.HeadRowNumber) {
         continue;
       }
-      for ( let column = rangeVal.s.c; column <= rangeVal.e.c; column++ ) {
-        let adr = xlsx.utils.encode_cell( { c: column, r: row } );
+      for (let column = rangeVal.s.c; column <= rangeVal.e.c; column++) {
+        let adr = xlsx.utils.encode_cell({ c: column, r: row });
         let cell = this.sheet[adr];
-        this.setMap( column, map, cell, adr )
+        this.setMap(column, map, cell, adr)
       }
       text += `{"index":{}}\n`;
       text += `{"text":"`;
-      Object.keys( config.excel.Columns ).forEach( function ( key ) {
-        text += `${map.get( this[key] )}`;
-        if ( maxColumnNumber !== key ) {
+      Object.keys(config.excel.Columns).forEach(function (key) {
+        text += `${map.get(this[key])}`;
+        if (maxColumnNumber !== key) {
           text += `${SEPARATOR}`;
         }
-      }, config.excel.Columns );
+      }, config.excel.Columns);
       text += `"}\n`;
       map.clear();
     }
@@ -48,30 +48,33 @@ class Excel {
   }
 
   getMaxColumnNumber() {
-    for ( var i in config.excel.Columns ) {
+    for (var i in config.excel.Columns) {
     }
     return i;
   }
 
-  setMap( column, map, cell, adr ) {
-    if ( column in config.excel.Columns ) {
-      if ( cell && cell.v ) {
-        map.set( config.excel.Columns[column], cell.v );
+  setMap(column, map, cell, adr) {
+    if (column in config.excel.Columns) {
+      if (cell && cell.v) {
+        map.set(config.excel.Columns[column], cell.v);
+        if (cell.v.indexOf("\r\n") > -1) {
+          console.log('改行あり:' + cell.v + ` ` + JSON.stringify(adr));
+        }
       } else {
-        map.set( config.excel.Columns[column], '' );
-        console.log( '空白のセル:' + JSON.stringify( adr ) );
+        map.set(config.excel.Columns[column], '');
+        console.log('空白のセル:' + JSON.stringify(adr));
       }
     }
   }
 
-  createJsonFile( excelData ) {
-    fs.writeFile( config.excel.OutputJsonFilePath, excelData, function ( err ) {
-      if ( err ) {
-        console.log( err );
+  createJsonFile(excelData) {
+    fs.writeFile(config.excel.OutputJsonFilePath, excelData, function (err) {
+      if (err) {
+        console.log(err);
       } else {
-        console.log( 'success' );
+        console.log('success');
       }
-    } );
+    });
   }
 }
 
